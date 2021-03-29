af78664c5a021943b8fc360a70220131420580d7
Hidde Ottens <hidde95@gmail.com>
2021-03-29 11:31:34 +0200

    feat: initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  api: {
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "4a60eb95e5a4425a9a5588812f3d0b92",
+    clientSecret: "aa987dd90ec34e8ea67d25736365e9c3",
+  },
+};
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routes from './routes';
+import CoreLayout from './common/layouts/CoreLayout';
+import './styles/_main.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoreLayout>
+      <Routes />
+    </CoreLayout>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
